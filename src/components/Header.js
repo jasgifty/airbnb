d6b0b372f1aea8aeb7bff9d61b48c502d505ed2f
gifty <jasmeetsingh200801@gmail.com>
2021-03-05 13:32:12 +0530

    core fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,6 @@ import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import LanguageIcon from "@material-ui/icons/Language";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { Avatar } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
 function Header() {
@@ -26,7 +25,6 @@ function Header() {
         <p>Become a host</p>
         <LanguageIcon />
         <ExpandMoreIcon />
-        <Avatar />
       </div>
     </div>
   );
